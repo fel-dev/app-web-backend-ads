99c84bbe5ba4474e6f583dd1d46adb66e638fbfb
User <User@DESKTOP-01BSJ5D>
2023-04-19 01:34:44 -0300

    Adicionar arquivos de projeto.

---
+++ app-web-backend/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace app_web_backend.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
