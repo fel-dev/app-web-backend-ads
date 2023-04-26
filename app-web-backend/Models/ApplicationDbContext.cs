462a6422a890d45fdeb7b6e31c186d684b312251
Felipe Corrêa Carneiro <correaflp@hotmail.com.br>
2023-04-26 01:08:32 -0300

    Configurado EntityFramework

---
+++ app-web-backend/Models/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace app_web_backend.Models {
+    public class ApplicationDbContext : DbContext { // Classe de contexto do Entity Framework Core
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) {
+
+        }
+        // models (objetos) que serão mapeados para o banco de dados
+    }
+}
--- app-web-backend/Program.cs
+++ app-web-backend/Program.cs
@@ -1,7 +1,14 @@
+using app_web_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
